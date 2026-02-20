4ef271e614c4c1d525866006565149c1d07f134d
Ermolaeva Daria <darerm1@yandex.ru>
2026-02-20 20:01:33 +0300

    feat: added icons for menu, favorites fragment and screen rotation

--- app/src/main/java/com/darerm1/whatcha/data/interfaces/MediaItem.kt
+++ app/src/main/java/com/darerm1/whatcha/data/interfaces/MediaItem.kt
@@ -1,6 +1,8 @@
 package com.darerm1.whatcha.data.interfaces
 
+import com.darerm1.whatcha.data.enums.Genre
 import com.darerm1.whatcha.data.enums.Status
+import java.time.LocalDate
 
 interface MediaItem {
     val id: Long
@@ -9,7 +11,11 @@ interface MediaItem {
 
     val year: Int
 
+    val genre: Genre
+
     var status: Status
 
     var personalRating: Int?
+
+    val date: LocalDate?
 }
